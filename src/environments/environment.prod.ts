3fba4e6150c33c63480c966b62e494cfa7c5da7b
tuminzee <t.sheth33@gmail.com>
2020-12-02 13:36:02 +0530

    Enviroment file changes for production and local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://sehatintelapituminzee.herokuapp.com/api',
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://sehatintelapituminzee.herokuapp.com/api',
+  // apiUrl: 'http://localhost:3000/api'
 };
