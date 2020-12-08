71cfb68b3c1f99b798c3cb7bb1f408b8871797d6
tuminzee <t.sheth33@gmail.com>
2020-12-09 00:15:42 +0530

    ❌ Remove login with google btn and feature

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,8 +5,6 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Users } from '../models/users';
 
-import firebase  from 'firebase/app';
-import { AngularFireAuth } from "@angular/fire/auth";
 
 
 @Injectable({
@@ -16,7 +14,6 @@ export class AuthService {
 
   private baseUrl = environment.apiUrl;
   constructor(private http: HttpClient,
-    public afAuth: AngularFireAuth,
     private router: Router) { }
 
   signupUser(user: Users): Observable<any>{
@@ -41,43 +38,4 @@ export class AuthService {
     this.router.navigate(['/login'])
   }
 
-  //  Sign in with Google
-   loginWithGoogle() {
-    return this.AuthLogin(new firebase.auth.GoogleAuthProvider());
-  }
-
-  // Auth logic to run auth providers
-    AuthLogin(provider) {
-      return firebase.auth().signInWithPopup(provider)
-      .then((result) => {
-        console.log(result)},
-        err => {
-          console.log(err);
-        }
-          // console.log(result)
-          // let user = {
-          //   email: result.user.email,
-          //   password: result.user.uid
-          // };
-          // this.loginUser(user).subscribe(
-          //   res => {
-          //     this.router.navigate(['/records'])
-          //     console.log(res)
-          //     localStorage.setItem('token', res.token )
-
-          //   },
-          //   err => console.log(err)
-          //   );
-      ).catch((error) => {
-          console.log(error)
-      })
-    }
-
-
-
-  // async googleSignin() {
-  //   const provider = new auth.GoogleAuthProvider();
-  //   const credential = await this.afAuth.auth.signInWithPopup(provider);
-  //   return this.updateUserData(credential.user);
-  // }
 }
